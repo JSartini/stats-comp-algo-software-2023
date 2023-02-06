810a8db7d92a2894753f0f6200b05a200ec532b1
Joseph Sartini <jsartin1@jhu.edu>
2023-02-05 22:29:37 -0500

    Added solution for scripting file required by Exercise 1

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -3,5 +3,8 @@
 for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
-  # Fill in
+  sed -i "$linenum i This is a sentence on line $linenum" foo.tex
+  pdflatex foo.tex > /dev/null 2>&1
+  git add -u
+  git commit -m "Adds a sentence at line $linenum and compiles"
 done
